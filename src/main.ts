a855cd5801f803d6e6cd36679d5f5b991ed21583
cyan0714 <917244295@qq.com>
2024-05-03 16:07:24 +0800

    feat: swagger 接口文档

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,26 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { FormatResponseInterceptor } from './format-response.interceptor';
 import { InvokeRecordInterceptor } from './invoke-record.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe())
-  app.useGlobalInterceptors(new FormatResponseInterceptor())
-  app.useGlobalInterceptors(new InvokeRecordInterceptor())
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new FormatResponseInterceptor());
+  app.useGlobalInterceptors(new InvokeRecordInterceptor());
+
+  const config = new DocumentBuilder()
+    .setTitle('会议室预订系统')
+    .setDescription('api 接口文档')
+    .setVersion('1.0')
+    .addBearerAuth({
+      type: 'http',
+      description: 'JWT 授权',
+    })
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-doc', app, document);
 
   const configService = app.get(ConfigService);
   await app.listen(configService.get('nest_server_port'));
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -17,7 +17,12 @@ import { ConfigService } from '@nestjs/config';
 import { UnauthorizedException } from '@nestjs/common';
 import { UserInfo, RequireLogin } from 'src/custom.decorator';
 import { UserDetailVo } from './vo/user-info.vo';
+import { ApiTags, ApiQuery, ApiBody, ApiResponse, ApiBearerAuth } from '@nestjs/swagger';
+import { HttpStatus } from '@nestjs/common';
+import { LoginUserVo } from './vo/login-user.vo';
+import { RefreshTokenVo } from './vo/refresh-token.vo';
 
+@ApiTags('用户管理模块')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
@@ -27,6 +32,17 @@ export class UserController {
   @Inject(ConfigService)
   private configService: ConfigService;
 
+  @ApiBody({ type: RegisterUserDto })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: '验证码已失效/验证码不正确/用户已存在',
+    type: String,
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: '注册成功/失败',
+    type: String,
+  })
   @Post('register')
   async register(@Body() registerUser: RegisterUserDto) {
     return await this.userService.register(registerUser);
@@ -39,12 +55,38 @@ export class UserController {
   }
 
   @Post('login')
+  @ApiBody({
+    type: LoginUserDto,
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: '用户不存在/密码错误',
+    type: String,
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: '用户信息和 token',
+    type: LoginUserVo,
+  })
   async userLogin(@Body() loginUser: LoginUserDto) {
     console.log('loginUser: ', loginUser);
     const user = await this.userService.login(loginUser, false);
     return user;
   }
 
+  @ApiBody({
+    type: LoginUserDto,
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: '管理员不存在/密码错误',
+    type: String,
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: '管理员信息和 token',
+    type: LoginUserVo,
+  })
   @Post('admin/login')
   async adminLogin(@Body() loginUser: LoginUserDto) {
     const user = await this.userService.login(loginUser, true);
@@ -52,6 +94,22 @@ export class UserController {
   }
 
   @Get('refresh')
+  @ApiQuery({
+    name: 'refreshToken',
+    type: String,
+    description: '刷新 token',
+    required: true,
+    example: 'xxxxxxxxyyyyyyyyzzzzz',
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'token 已失效，请重新登录',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: '刷新成功',
+    type: RefreshTokenVo,
+  })
   async refresh(@Query('refreshToken') refreshToken: string) {
     try {
       const data = this.jwtService.verify(refreshToken);
@@ -60,16 +118,35 @@ export class UserController {
       const access_token = this.userService.getToken(user).accessToken;
       const refresh_token = this.userService.getToken(user).refreshToken;
 
-      return {
-        access_token,
-        refresh_token,
-      };
+      const vo = new RefreshTokenVo();
+
+      vo.access_token = access_token;
+      vo.refresh_token = refresh_token;
+
+      return vo;
     } catch (e) {
       throw new UnauthorizedException('token 已失效，请重新登录');
     }
   }
 
+
   @Get('admin/refresh')
+  @ApiQuery({
+    name: 'refreshToken',
+    type: String,
+    description: '刷新 token',
+    required: true,
+    example: 'xxxxxxxxyyyyyyyyzzzzz',
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'token 已失效，请重新登录',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: '刷新成功',
+    type: RefreshTokenVo,
+  })
   async adminRefresh(@Query('refreshToken') refreshToken: string) {
     try {
       const data = this.jwtService.verify(refreshToken);
@@ -88,6 +165,12 @@ export class UserController {
   }
 
   @Get('info')
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'success',
+    type: UserDetailVo,
+  })
+  @ApiBearerAuth()
   @RequireLogin()
   async info(@UserInfo('userId') userId: number) {
     const user = await this.userService.findUserDetailById(userId);
@@ -101,7 +184,7 @@ export class UserController {
     vo.nickName = user.nickName;
     vo.createTime = user.createTime;
     vo.isFrozen = user.isFrozen;
-    
+
     return vo;
   }
 }
